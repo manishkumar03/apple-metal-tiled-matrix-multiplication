f5f9076a1602aacfac9713ba37a337af04f8c180
Manish Kumar <manish.bansal@gmail.com>
2025-07-06 13:40:08 -0400

    Initial Commit

---
+++ TiledMatrixMultiplication/TiledMatrixMultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiledMatrixMultiplicationApp.swift
+//  TiledMatrixMultiplication
+//
+//  Created by Manish Kumar on 2025-07-06.
+//
+
+import SwiftUI
+
+@main
+struct TiledMatrixMultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
